bfa61ca3658a3f92e40a06bbd695f447dc5ee074
yangguangyong <yangguangyong@xueqiu.com>
2022-01-13 21:22:35 +0800

    init

--- src/com/xueqiu/infra/cd/Git.groovy
+++ src/com/xueqiu/infra/cd/Git.groovy
@@ -4,7 +4,8 @@ def clone(branch) {
     log.i '分支:' + branch
     sh("pwd")
     sh("git checkout ${branch}")
-    log.i 'git clone完成'
+    sh 'mvn clean package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -gs /home/jenkins/settings/settings.xml'
+    log.i '打包完成'
 }
 
 return this
