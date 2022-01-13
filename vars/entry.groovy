69ae5e3844c31261afc3864eeb85f66d9ea7d6ef
yangguangyong <yangguangyong@xueqiu.com>
2022-01-13 21:09:18 +0800

    init

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -6,6 +6,37 @@ def call(Map config=[:]) {
 
    def git = new Git()
 
+    parameters {
+        gitParameter (
+                branch: '',
+                branchFilter: 'origin/(.*)',
+                defaultValue: 'master',
+                listSize: '10',
+                name: 'GIT_REVISION',
+                quickFilterEnabled: true,
+                selectedValue: 'NONE',
+                sortMode: 'DESCENDING_SMART',
+                tagFilter: '*',
+                type: 'PT_BRANCH_TAG',
+                description: 'Please select a branch or tag to build',
+                useRepository: Config.data.git_repo)
+
+        choice(
+                name: 'ENVIRONMENT',
+                description: 'Please select Environment',
+                choices: 'dev\nqa\nuat\npre\nprd')
+
+        choice(
+                name: 'ACTION',
+                description: 'Please select action',
+                choices: 'deploy\nrollback')
+
+        choice(
+                name: 'DEPLOYMENT_MODE',
+                description: 'Please select action',
+                choices: 'Container\nLegacy\nMixed')
+    }
+
     pipeline
             {
                 agent
