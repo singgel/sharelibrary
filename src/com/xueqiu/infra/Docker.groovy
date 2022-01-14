b4211629f4ffdbee50da7deb345f131fccafefa5
yangguangyong <yangguangyong@xueqiu.com>
2022-01-14 13:52:18 +0800

    init

--- src/com/xueqiu/infra/Docker.groovy
+++ src/com/xueqiu/infra/Docker.groovy
@@ -5,11 +5,12 @@ def build(projectName,version,environment) {
     log.i '开始Docker镜像构建'
     //执行部署脚本
     def deploymentFile = libraryResource("k8s/deployment.yaml")
+    writeFile file: './deployment.yaml', text: deploymentFile
+    sh "cat ./deployment.yaml"
+
     sh "ls -l ${projectName}/target"
-    sh "echo `sed '#{{PROJECT_NAME}}#${projectName}#g' ${deploymentFile}` >> ./deployment.yaml"
-    sh "cat ./deployment.yaml"
+    sh "sed -i '#{{PROJECT_NAME}}#${projectName}#g' ./deployment.yaml"
     sh "sed -i '#{{ENVIRONMENT}}#${environment}#g' ./deployment.yaml"
-    sh "cat ./deployment.yaml"
     sh "docker login xq-harbor-ingress.ce027df6a3ed8476bb82b2cd0e6f6f219.cn-beijing.alicontainer.com -u admin -p Xq-Harbor-Aliyun-K8s"
     sh "docker build -t lib/${projectName}:${version} -f docker/Dockerfile ."
     sh "docker images"
