2194d8cfecc6a4e098982c3c3a777b979c897dc9
yangguangyong <yangguangyong@xueqiu.com>
2022-01-14 16:24:38 +0800

    init

--- src/com/xueqiu/infra/Git.groovy
+++ src/com/xueqiu/infra/Git.groovy
@@ -17,14 +17,9 @@ def clone(branch,repo,credentialsId) {
 def build() {
  try {
     log.i '开始打包'
-    sh("pwd")
-
-     def mavenFile = libraryResource("apache-maven-3.8.4-bin.zip")
-     writeFile file: './maven.zip', text: mavenFile
-     sh "unzip ./maven.zip"
-
-     sh 'apache-maven-3.8.4/bin/mvn clean package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -gs /home/jenkins/settings/settings.xml'
-    log.i '打包完成'
+     sh("pwd")
+     sh 'mvn clean package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -gs /home/jenkins/settings/settings.xml'
+     log.i '打包完成'
  }catch(e) {
     log.e 'Ops! Error occurred during mvn package'
     throw e
