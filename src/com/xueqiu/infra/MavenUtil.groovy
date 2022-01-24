c8cd3cdd8b9f9d5a7357ca51933cb60af71ac9a0
shiwenjie <shiwenjie@xueqiu.com>
2022-01-24 10:52:31 +0800

    test maven

--- src/com/xueqiu/infra/MavenUtil.groovy
+++ src/com/xueqiu/infra/MavenUtil.groovy
@@ -5,6 +5,8 @@ def Unzip() {
     try {
         sh "pwd"
         sh "ls"
+        sh "unzip ./resources/apache-maven-3.6.1-bin.zip /usr/local"
+        sh "ls /usr/local"
     }
     catch (e) {
         throw e
